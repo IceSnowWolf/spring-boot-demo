d15790bbaae075b1647dd92ae9872bf8e2244dbc
wang.xiaoguang <wang.xiaoguang@fujisoft-china.com>
2020-06-22 10:37:04 +0800

    定制错误页面

---
+++ src/main/java/com/lmq/demo/base/ErrorPageController.java
@@ -0,0 +1,20 @@
+package com.lmq.demo.base;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error")
+public class ErrorPageController {
+
+    @RequestMapping("/404")
+    public String error404(){
+        return "/error/404";
+    }
+
+    @RequestMapping("/500")
+    public String error500(){
+        return "/error/500";
+    }
+
+}
